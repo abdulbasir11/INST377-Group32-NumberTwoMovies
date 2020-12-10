f8f82249ce50ea9da8cb6ec547a28c3eefba8182
abdulbasir11 <abdulbasirmusician@gmail.com>
2020-12-10 04:08:48 -0500

    modified routes and added endpoints

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
+
 const app = express();
 var serverPort = 3000;
 var port = process.env.PORT || serverPort;
+const dev = process.env.NODE_ENV !== 'production';
+const server = dev ? 'http://localhost:3000' : 'https://number-two-movies.herokuapp.com/';
 
 /* this line is for heroku deployments
 if (process.env.NODE_ENV === 'production') {
@@ -14,14 +17,14 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('./'));
-app.use(express.static('./public'));
-/*
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-*/
+
 
 app.route('/').
   get((req, res) => {
@@ -31,13 +34,23 @@ app.route('/').
     console.log('POST request detected for main route')
   });
 
-  //test GET route that sends some random HTML
-  app.get('/test', (req, res) => {
-    let name = req.query.name
-    let gender = req.query.gender
-    res.send("Hello!"+name+"! You are a"+gender)
-    console.log('GET request detected for private route')
-  });
+app.route('/results')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    console.log('fetch request data', json)
+  })
+  .post(async (req, res) => {
+
+    const getMovie = await fetch(`${server}/server_files/movieList.txt`)
+    const movielist = await getMovie.text();
+    let lines = movielist.split('\n');
+    let movie = lines[Math.floor(Math.random()*lines.length)];
+
+    console.log('POST request detected');
+    const data = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=5606a178&t="+movie);
+    const json = await data.json();
+    res.json(json);
+  })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
