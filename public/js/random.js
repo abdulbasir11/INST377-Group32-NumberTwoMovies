d700c92ade8468dcab823ee73e6eb762dfa34fde
jojokimys <jojokimys@users.noreply.github.com>
2020-12-12 08:30:09 +0900

    selection page

--- public/js/random.js
+++ public/js/random.js
@@ -10,6 +10,14 @@ async function getRandomMovies() {
     .then(data => {
         console.log('Success:', data);
         $("#poster-img").attr("src",data.Poster);
+        $(".movietitle").text(data.Title);
+        
+        $(".imdbrating p").text(() => $(".imdbrating").text() + data.imdbRating);
+        data.Ratings.map((item, index) => {
+            $(".ratings_section").append(`<li><p>${data.Ratings[index].Source}: ${data.Ratings[index].Value}</p></li>`);
+        })
+        
+        $(".mpaarating").text(() => $(".mpaarating").text() + data.Rated);
     })
 }
 
